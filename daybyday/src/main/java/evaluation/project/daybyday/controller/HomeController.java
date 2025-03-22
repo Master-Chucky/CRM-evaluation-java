42a6ca0fd3205900ddc6a13c87b4a0a537af9ba7
ANDRIAMANANTENA Mamitiana Rianala <mamitianarianala404@gmail.com>
2025-03-22 12:34:52 +0300

    finalisation view login

--- daybyday/src/main/java/evaluation/project/daybyday/controller/HomeController.java
+++ daybyday/src/main/java/evaluation/project/daybyday/controller/HomeController.java
@@ -4,12 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import evaluation.project.daybyday.dto.LoginDTO;
+
 @Controller
 public class HomeController {
     
     @GetMapping("/")
     public String index(Model model) {
-        return "redirect:auth/login";
+        LoginDTO loginDTO = new LoginDTO();
+        model.addAttribute("loginDTO", loginDTO);
+        return "auth/login";
     }
 
 }
