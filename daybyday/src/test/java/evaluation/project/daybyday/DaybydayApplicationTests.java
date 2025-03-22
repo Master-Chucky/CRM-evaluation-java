836fe15438a1bbfe24d4a339fb21ec03f991ad8d
ANDRIAMANANTENA Mamitiana Rianala <mamitianarianala404@gmail.com>
2025-03-22 09:34:12 +0300

    first commit

---
+++ daybyday/src/test/java/evaluation/project/daybyday/DaybydayApplicationTests.java
@@ -0,0 +1,13 @@
+package evaluation.project.daybyday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaybydayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
