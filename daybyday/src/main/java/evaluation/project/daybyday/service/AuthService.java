8d099ebc9a945b50407c15e4f7a9865ad262a657
ANDRIAMANANTENA Mamitiana Rianala <mamitianarianala404@gmail.com>
2025-03-22 10:33:55 +0300

    config + authService

---
+++ daybyday/src/main/java/evaluation/project/daybyday/service/AuthService.java
@@ -0,0 +1,43 @@
+package evaluation.project.daybyday.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class AuthService {
+    
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public AuthService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public String authenticate(String email, String password) {
+        String url = "http://ton-api-laravel.com/api/authenticate";
+        
+        Map<String, String> requestBody = new HashMap<>();
+        requestBody.put("email", email);
+        requestBody.put("password", password);
+        
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+        
+        HttpEntity<Map<String, String>> requestEntity = new HttpEntity<>(requestBody, headers);
+        
+        @SuppressWarnings("rawtypes")
+        ResponseEntity<Map> response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, Map.class);
+
+        if (response.getBody() != null && response.getBody().containsKey("token")) {
+            return (String) response.getBody().get("token");
+        }
+        return null;
+    }
+}
