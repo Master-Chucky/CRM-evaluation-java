b3666d66ce24d3c0283606e1898bd9f104a2edba
ANDRIAMANANTENA Mamitiana Rianala <mamitianarianala404@gmail.com>
2025-03-23 21:05:12 +0300

    invoice data

---
+++ daybyday/src/main/java/evaluation/project/daybyday/util/InvoiceResponseDTO.java
@@ -0,0 +1,49 @@
+package evaluation.project.daybyday.util;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import evaluation.project.daybyday.dto.InvoiceDTO;
+import lombok.Data;
+
+@Data
+public class InvoiceResponseDTO {
+    
+    @JsonProperty("current_page")
+    private int currentPage;
+
+    @JsonProperty("data")
+    private List<InvoiceDTO> data;
+
+    @JsonProperty("first_page_url")
+    private String firstPageUrl;
+
+    @JsonProperty("from")
+    private int from;
+
+    @JsonProperty("last_page")
+    private int lastPage;
+
+    @JsonProperty("last_page_url")
+    private String lastPageUrl;
+
+    @JsonProperty("next_page_url")
+    private String nextPageUrl;
+
+    @JsonProperty("path")
+    private String path;
+
+    @JsonProperty("per_page")
+    private int perPage;
+
+    @JsonProperty("prev_page_url")
+    private String prevPageUrl;
+
+    @JsonProperty("to")
+    private int to;
+
+    @JsonProperty("total")
+    private int total;
+
+}
